2d5d08a29629c572ea1852492678ca3d59130443
Bilal Khan <bilal.khan@live.com>
2024-07-25 20:19:54 +1000

    Initial commit word analytics

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Container from "./components/Container";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
