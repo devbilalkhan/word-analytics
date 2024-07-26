3cbd08da866c835c3c9c62dcda217e5f9dc0641c
Bilal Khan <bilal.khan@live.com>
2024-07-26 15:25:20 +1000

    warning component css adjustments

--- src/components/Warning.jsx
+++ src/components/Warning.jsx
@@ -1,6 +1,6 @@
 export default function Warning({ message }) {
   return (
-    <p className="px-6 text-red-500 absolute bottom-1">
+    <p className="px-6 text-red-500 absolute bottom-[16.5rem] md:bottom-1">
       <small>{message}</small>
     </p>
   );
